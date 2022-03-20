a5fb374a49fdbc6284ebc09a50c61b18bbe99aca
javierantonme <javierantonme@gmail.com>
2022-03-20 15:48:19 -0500

    primer commit

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = ()=>{
+    return(
+        <h1>Javier Melo</h1>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'build.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: './public/index.html',
+                filname: './index.html'
+            }
+        ),
+    ],
+}
