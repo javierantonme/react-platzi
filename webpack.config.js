dd7dc0666a254f6a23f1ece8d70d91eb2e1244e5
javierantonme <javierantonme@gmail.com>
2022-03-22 21:08:27 -0500

    todo corregido

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|jp(e*)g|svg|gif)$/
+                test: /\.(png|jpg|svg|gif)$/
             }
         ]
 },
